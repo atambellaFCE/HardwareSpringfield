3fb41407a09908338c639c4da400537e060771de
jprociuk <16PR40673261@campus.economicas.uba.ar>
2023-06-19 10:44:06 -0300

    Estructura de main y metodos

--- HardwareSpringfield.Consola/Program.cs
+++ HardwareSpringfield.Consola/Program.cs
@@ -11,9 +11,14 @@ namespace HardwareSpringfield.Consola
 {
     class Program
     {
+
+
         public static void Main(string[] args)
         {
+            int usuario;
+
             Console.WriteLine("Hola Mundo,");
+
 
             ClienteNegocio clienteNegocio = new ClienteNegocio();
             clienteNegocio.crearCliente();
@@ -25,11 +30,12 @@ namespace HardwareSpringfield.Consola
             Thread.Sleep(2500);
 
             Console.WriteLine("Bienvenido a SpringField - Venta de Hardware!\n");
-            MenuPrincipal();
+
             Console.ReadKey();
         }
-            public static void MenuPrincipal()
+        public static void MenuPrincipal()
             {
+
                 Console.WriteLine("Menu principal");
                 Console.WriteLine("0 - Salir del sistema");
                 Console.WriteLine("1 - Menu Cliente");
@@ -53,51 +59,50 @@ namespace HardwareSpringfield.Consola
                         case 2:
                             MenuProducto();
                             break;
+                        case 3:
+                            MenuVentas();
+                            break;
+                        case 4:
+                            MenuProveedores();
+                            break;
+                        case 5;
+                            MenuReportes();
+                            break;
                     }
                 } while (valor != 0);
             }
 
-        public static void MenuCliente()
-        {
-            if (Cliente == null)
+            public static void MenuCliente()
             {
-                Cliente = new Cliente();
-                //cliente.XXX = Validaciones.PedirInt("Por favor ingrese su numero:", 100000, 999999);
-                Console.Clear();
-            }
-            Console.WriteLine($"Usuario: Cliente - numero {jefe_De_Catedra.NroLegajo}\n");
-            Console.WriteLine("0 - Volver al menu principal");
-            Console.WriteLine("1 -");
-            Console.WriteLine("2 - l");
-            Console.WriteLine("3 -");
-            Console.WriteLine("4 -");
+                if (Cliente == null)
+                {
+                    Cliente = new Cliente();
+                    //cliente.XXX = Validaciones.PedirInt("Por favor ingrese su numero:", 100000, 999999);
+                    Console.Clear();
+                }
+                //Console.WriteLine($"Usuario: Cliente - numero {}\n");
+                Console.WriteLine("0 - Volver al menu principal");
+                Console.WriteLine("1 - Ingresar Cliente");
+                Console.WriteLine("2 - Consultar Clientes");
 
-            int valor;
-            valor = Validaciones.PedirInt("\nSeleccione una opcion:", 0, 4);
-            Console.Clear();
-            switch (valor)
-            {
-                case 0:
-                    MenuPrincipal();
-                    break;
-                case 1:
-                    //cliente.ListarDatos(DatosAlumnos);
-                    Console.Clear();
-                    break;
-                case 2:
-                    // jefe_De_Catedra.Cant(DatosAlumnos);
-                    Console.Clear();
-                    break;
-                case 3:
-                    //jefe_De_Catedra.EditarAlumno(DatosAlumnos);
-                    Console.Clear();
-                    break;
-                case 4:
-                    //jefe_De_Catedra.BuscarRegistro(DatosAlumnos);
-                    Console.Clear();
-                    break;
+                int valor;
+                valor = Validaciones.PedirInt("\nSeleccione una opcion:", 0, 4);
+                Console.Clear();
+                switch (valor)
+                {
+                    case 0:
+                        MenuPrincipal();
+                        break;
+                    case 1:
+                        //Clientes.ListarClientes();
+                        Console.Clear();
+                        break;
+                    case 2:
+                        //Clientes.ConsultarClientes();
+                        Console.Clear();
+                        break;
+                }
             }
-        }
             public static void MenuProducto()
             {
                 if (Producto == null)
@@ -122,22 +127,114 @@ namespace HardwareSpringfield.Consola
                         MenuPrincipal();
                         break;
                     case 1:
-                        //cliente.ListarDatos(DatosAlumnos);
+                        //Ingresar productos;
                         Console.Clear();
                         break;
                     case 2:
-                        // jefe_De_Catedra.Cant(DatosAlumnos);
-                        Console.Clear();
-                        break;
-                    case 3:
-                        //jefe_De_Catedra.EditarAlumno(DatosAlumnos);
-                        Console.Clear();
-                        break;
-                    case 4:
-                        //jefe_De_Catedra.BuscarRegistro(DatosAlumnos);
+                        //Consultar productos;
                         Console.Clear();
                         break;
                 }
             }
-        }
+            public static void MenuProveedores()
+            {
+                if (Proveedor == null)
+                {
+                    Producto = new Producto();
+                    //cliente.XXX = Validaciones.PedirInt("Por favor ingrese su numero:", 100000, 999999);
+                    Console.Clear();
+                }
+                Console.WriteLine($"Usuario: Cliente - numero \n");
+                Console.WriteLine("0 - Volver al menu principal");
+                Console.WriteLine("1 -");
+                Console.WriteLine("2 - l");
+                Console.WriteLine("3 -");
+                Console.WriteLine("4 -");
+
+                int valor;
+                valor = Validaciones.PedirInt("\nSeleccione una opcion:", 0, 4);
+                Console.Clear();
+                switch (valor)
+                {
+                    case 0:
+                        MenuPrincipal();
+                        break;
+                    case 1:
+                        //Ingresar Proveedores;
+                        Console.Clear();
+                        break;
+                    case 2:
+                        //consultar Proveedores;
+                        Console.Clear();
+                        break;
+                }
+            }
+
+            public static void MenuVentas()
+        {
+            {
+                if (Venta == null)
+                {
+                    Venta = new Venta();
+                    //cliente.XXX = Validaciones.PedirInt("Por favor ingrese su numero:", 100000, 999999);
+                    Console.Clear();
+                }
+                Console.WriteLine($"Usuario: Cliente - numero \n");
+                Console.WriteLine("0 - Volver al menu principal");
+                Console.WriteLine("1 -");
+                Console.WriteLine("2 - l");
+
+                int valor;
+                valor = Validaciones.PedirInt("\nSeleccione una opcion:", 0, 4);
+                Console.Clear();
+                switch (valor)
+                {
+                    case 0:
+                        MenuPrincipal();
+                        break;
+                    case 1:
+                        //Ingresar Ventas;
+                        Console.Clear();
+                        break;
+                    case 2:
+                        // Consultar ventas;
+                        Console.Clear();
+                        break;
+                
+            public static void MenuReportes()
+            {
+                {
+                    if (Venta == null)
+                    {
+                        Venta = new Venta();
+                        //cliente.XXX = Validaciones.PedirInt("Por favor ingrese su numero:", 100000, 999999);
+                        Console.Clear();
+                    }
+                    Console.WriteLine($"Usuario: Cliente - numero \n");
+                    Console.WriteLine("0 - Volver al menu principal");
+                    Console.WriteLine("1 -");
+                    Console.WriteLine("2 - l");
+
+                    int valor;
+                    valor = Validaciones.PedirInt("\nSeleccione una opcion:", 0, 4);
+                    Console.Clear();
+                    switch (valor)
+                    {
+                        case 0:
+                            MenuPrincipal();
+                            break;
+                        case 1:
+                            //Ingresar Ventas;
+                            Console.Clear();
+                            break;
+                        case 2:
+                            // Consultar ventas;
+                            Console.Clear();
+                            break;
+                    }
+                }
+
+
+            }
     }
+}
---
+++ HardwareSpringfield.Negocio/ReportesNegocio.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace HardwareSpringfield.Negocio
+{
+	public class ReportesNegocio
+	{
+		public ReportesNegocio()
+		{
+		}
+	}
+}
+
